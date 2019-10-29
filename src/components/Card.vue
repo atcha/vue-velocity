aa39ea8030ec71420edec93c052b2029ad9ff2cb
atcha <francois.cornuaud@mutuelledepoitiers.fr>
2019-10-29 15:39:31 +0100

    Add Card component / Add a card to home page

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="flex p-10 bg-white shadow-2l">
+        <img v-if="img"
+             class="mr-10"
+             :src="img.url"
+             :alt="img.alt"
+             :width="img.width"
+             :height="img.height">
+        <div class="flex flex-col justify-center items-start">
+            <h2 class="text-black-1 text-xl" v-if="title">{{ title }}</h2>
+            <p class="text-gray-1" v-if="content">{{ content }}</p>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "Card",
+        props: {
+            img: Object,
+            title: String,
+            content: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,28 @@
 <template>
-    <div class="">
+    <div class="flex p-20">
+        <Card :img="operatingCard.img" :title="operatingCard.title" :content="operatingCard.content"></Card>
     </div>
 </template>
 
 <script>
+    import Card from "../components/Card";
     export default {
         name: 'home',
+        components: { Card },
+        data () {
+            return {
+                operatingCard: {
+                    img: {
+                        url: require('../assets/img/home/operating-score.png'),
+                        alt: 'Operating Score',
+                        width: 203,
+                        height: 142
+                    },
+                    title: 'Welcome to Velocity',
+                    content: 'All cars are operating well. There were 1,233 trips since your last login.'
+                }
+            }
+        }
     }
 </script>
 
